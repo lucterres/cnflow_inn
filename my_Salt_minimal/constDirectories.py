1b59f0d04398fe5217ef04714c6add7169cc9edc
Luciano <lucianoterres@gmail.com>
2023-02-24 21:53:29 -0300

    inspect seismic data loader

--- my_Salt_minimal/constDirectories.py
+++ my_Salt_minimal/constDirectories.py
@@ -1,12 +1,11 @@
-dataSetDIR = f"D:\_0Luciano\_0PHD\datasets"
-masksDir =  f'D:\_0Luciano\_0PHD\datasets\masks10-90'
+
+root = 'E:\Luciano\_0PH\Datasets/tgs-salt'
 
 
-TRAIN_MASK_DIR = './train/'
-TRAIN_CSV = f'D:/_0Luciano/_0PHD/datasets/tgs-salt/train1090.csv'    #./train/train.csv'
+TRAIN_CSV = root + '/train1090.csv'    
 DEPTH_CSV = 'depths.csv'
 
-TRAIN_DIR  = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/'
-TRAIN_IMAGE_DIR = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/train/images/'
-TRAIN_MASK_DIR  = 'D:/_0Luciano/_0PHD/datasets/tgs-salt/train/masks/'
-TEST_IMAGE_DIR  = 'D:/_0Luciano_0PHD/datasets/tgs-salt/test/images/'
+TRAIN_DIR  = root + '/train'
+TRAIN_MASK_DIR  = TRAIN_DIR + '/masks/'
+TRAIN_IMAGE_DIR = TRAIN_DIR + '/images/'
+TEST_IMAGE_DIR  = root + '/test/images/'
--- my_Salt_minimal/dataSeismic.py
+++ my_Salt_minimal/dataSeismic.py
@@ -15,7 +15,7 @@ data_std = 0.305
 
 # amplitude for the noise augmentation
 augm_sigma = 0.08
-data_dir = 'D:\_0Luciano\_0PHD\datasets' #'E:\Luciano\_0PH\Datasets'  #'mnist_data'
+data_dir = ct.root  #'mnist_data'
 
 def unnormalize(x):
     '''go from normaized data x back to the original range'''
@@ -45,8 +45,6 @@ class SeismicImageDataset(Dataset):
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
-        if self.label_transform:
-            label = self.label_transform(label)
         return image, label
 
 
@@ -63,22 +61,3 @@ transform = transforms.Compose([transforms.Resize((28 , 28)),
 train_dataSeismic = SeismicImageDataset(ct.TRAIN_CSV,ct.TRAIN_IMAGE_DIR, transform)
 
 train_loaderSeismic = DataLoader(train_dataSeismic, batch_size=64, shuffle=True)
-
-
-
-train_data = torchvision.datasets.MNIST(data_dir, train=True, download=True, transform=transf)
-test_data = torchvision.datasets.MNIST(data_dir, train=False, download=True, transform=transf)
-
-# Sample a fixed batch of 1024 validation examples
-val_x, val_l = zip(*list(train_data[i] for i in range(1024)))
-val_x = torch.stack(val_x, 0).cuda()
-val_l = torch.LongTensor(val_l).cuda()
-
-# Exclude the validation batch from the training data
-train_data.data = train_data.data[1024:]
-train_data.targets = train_data.targets[1024:]
-# Add the noise-augmentation to the (non-validation) training data:
-train_data.transform = T.Compose([train_data.transform, T.Lambda(noise)])    
-
-train_loader  = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
-test_loader   = DataLoader(test_data,  batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True, drop_last=True)
